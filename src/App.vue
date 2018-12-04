748409cd909e42d70ef00d2f29bc43e3452f867d
julietaflux <julietaflux@gmail.com>
2018-12-04 19:31:46 -0300

    hi there

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    <v-container align-center>
+     <div>
+       <h1>Blood Pressure</h1>
+        <v-card>
+          <SomeInfoChart ref="refChart" :title="myTitle" />
+        </v-card>
+      </div>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import SomeInfoChart from './components/SomeInfoChart'
+
+export default {
+  name: 'App',
+  components: {
+    SomeInfoChart
+  },
+  data () {
+    return {
+      myTitle: "Blood pressure"
+    }
+  },
+}
+</script>
+
+<style lang="css" scoped>
+div {
+  text-align: center;
+}
+
+h1 {
+  padding: 1rem;
+}
+</style>
+
---
+++ src/components/SomeInfoChart.vue
@@ -0,0 +1,40 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  props: ['title'],
+  methods: {
+      refresh: function(labels, highPressure, lowPressure) {
+            let self = this;
+            this.renderChart({
+                labels: labels,
+                datasets: [
+                    {
+                        label: 'High Pressure',
+                        backgroundColor: 'rgb(153, 0, 0)',
+                        borderColor: 'rgb(153, 0, 0)',
+                        data: highPressure,
+                        borderWidth: 2,
+                        fill: false,
+                        lineTension: 0,
+                    },
+                    {
+                        label: 'Low Pressure',
+                        backgroundColor: 'rgb(51, 51, 153)',
+                        borderColor: 'rgb(51, 51, 153)',
+                        data: lowPressure,
+                        borderWidth: 2,
+                        fill: false,
+                        lineTension: 0,
+                    }
+                ]
+            }, 
+        {responsive: true, maintainAspectRatio: false})    
+    }
+  },
+  mounted () {
+      this.refresh(["L" , "M", "M", "J", "V", "S", "D"], [8, 9, 8, 7, 6, 7, 7], [11, 11, 13, 12, 11, 13, 10]);  
+  }, 
+}
+</script>
